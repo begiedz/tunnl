68eb73a6c559413661cf8dcd2e07e2761ecf9b68
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-08-22 22:45:57 +0200

    add message component

--- components/chat/message-field.tsx
+++ components/chat/message-field.tsx
@@ -1,5 +1,40 @@
+import Message from './message'
+
 const MessageField = () => {
-  return <section>MessageField</section>
+  return (
+    <section className="flex h-full w-full flex-col overflow-scroll">
+      <Message value="elo" sentTime={2} />
+      <Message value="?" sentTime={5} />
+      <Message owner value="No siemano!" sentTime={20} />
+      <Message value="No siemano!" />
+      <Message owner value="co tam" />
+      <Message value="gitarka" />
+      <Message />
+      <Message img="image.jpg" value="xD" />
+      <Message
+        value="
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit optio numquam modi adipisci repellendus ex perspiciatis, fugit corrupti recusandae. Assumenda!
+      "
+      />
+      <Message
+        owner
+        value="
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit optio numquam modi adipisci repellendus ex perspiciatis, fugit corrupti recusandae. Assumenda!
+      "
+      />
+      <Message
+        owner
+        value="
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit optio numquam modi adipisci
+      "
+      />
+      <Message owner value="👍" />
+      <Message owner value="👍" />
+      <Message owner value="👍" />
+      <Message owner value="👍" />
+      <Message owner value="👍" />
+    </section>
+  )
 }
 
 export default MessageField
---
+++ components/chat/message.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { cn } from '@/lib/utils'
+
+interface MessageProps {
+  owner?: boolean
+  value?: string
+  sentTime?: number
+  img?: string
+}
+
+const Message = ({ owner, value, img, sentTime }: MessageProps) => {
+  return (
+    <div
+      className={cn('m-2 flex max-w-[80%] flex-col gap-2', owner && 'self-end')}
+    >
+      <div className="flex gap-2">
+        {!owner && (
+          <Avatar className="size-6 self-end">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>UP</AvatarFallback>
+          </Avatar>
+        )}
+
+        <div
+          className={cn(
+            'border border-input bg-background px-4 py-2',
+            'rounded-bl rounded-br-3xl rounded-tl-3xl rounded-tr-3xl',
+            owner && 'rounded-bl-3xl rounded-br border-none bg-black text-white'
+          )}
+        >
+          {img && <img src={img} className="rounded-3xl object-cover" />}
+          {value}
+        </div>
+      </div>
+      {!!sentTime && (
+        <span
+          className={cn(
+            'w-auto text-xs font-semibold text-black/50',
+            owner && 'self-end'
+          )}
+        >
+          {sentTime} min ago
+        </span>
+      )}
+    </div>
+  )
+}
+
+export default Message
